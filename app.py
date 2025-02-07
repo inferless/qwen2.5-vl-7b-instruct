00e75a45467d1b72e5b7d92587e7c6cd590bac2c
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-02-07 17:51:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from qwen_vl_utils import process_vision_info
 
 class InferlessPythonModel:
   def initialize(self):
-        self.llm = LLM(model="Qwen/Qwen2-VL-7B-Instruct")
-        self.processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct")
+        self.llm = LLM(model="Qwen/Qwen2.5-VL-7B-Instruct")
+        self.processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
       
   def infer(self, inputs):
         prompt = inputs["prompt"]
